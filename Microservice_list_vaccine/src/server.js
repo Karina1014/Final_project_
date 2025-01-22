1ffef5e99be79c57822e80917306b7e50b7e71bf
Jesdhy <pinchajessy529@gmail.com>
2025-01-22 18:59:26 -0500

    feat: :hammer: add Swagger
    add Swagger configuration for API list vaccine documentation

--- Microservice_list_vaccine/src/server.js
+++ Microservice_list_vaccine/src/server.js
@@ -3,11 +3,16 @@ import cors from 'cors';
 import vaccineRoute from './routes/vaccineRouter.js';
 
 const app = express();
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert { type: "json" };
+
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', vaccineRoute);
 
 app.listen(PORT, () => {
