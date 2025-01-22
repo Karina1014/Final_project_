c0e97644b8a1280142cae82ff19c76ba277f1cc7
Jesdhy <pinchajessy529@gmail.com>
2025-01-22 01:13:30 -0500

    feat: :sparkles: Add new files
    Add new files for microservice delete vaccine

---
+++ Microservice_delete_vaccine/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import vaccineRouter from './routes/vaccineRouter.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', vaccineRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
+
+
+
