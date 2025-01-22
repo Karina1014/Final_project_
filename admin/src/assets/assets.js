56bf02546dfcfb6ca0a5e281b395a16dd7f3ad59
Karina1014 <janneth_wi15@hotmail.com>
2025-01-21 19:44:52 -0500

    feat: :sparkles: add admin folder
    add new folder frontend

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import background1 from './background1.png'
+import background2 from './background2.jpg'
+import background3 from './background3.png'
+import dermatology from './dermatology.png'
+import arrow_icon from './arrow_icon.svg'
+import logo from './logo.png'
+import dogBackground from './dogBackground.jpg'
+export const assets = {
+    background1,
+    background2,
+    background3,
+    arrow_icon,
+    logo,
+    dogBackground
+}
