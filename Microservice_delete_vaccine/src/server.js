2397eea45775ca29bd5607f1326845c17a93bd4f
Jesdhy <pinchajessy529@gmail.com>
2025-01-22 19:50:07 -0500

    feat: :hammer: add Swagger
    add Swagger configuration for API delete vaccine documentation

--- Microservice_delete_vaccine/src/server.js
+++ Microservice_delete_vaccine/src/server.js
@@ -3,11 +3,16 @@ import cors from 'cors';
 import vaccineRouter from './routes/vaccineRouter.js';
 
 const app = express();
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert { type: "json" };
+
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', vaccineRouter);
 
 app.listen(PORT, () => {
