a509ba4065ec71f748bea3e24051427ef2f2685f
Karina1014 <janneth_wi15@hotmail.com>
2025-02-09 22:40:51 -0500

    feat: :sparkles: Prove changes
    Prove changes ip

--- vaccine_management/Microservice_list_vaccine/src/server.js
+++ vaccine_management/Microservice_list_vaccine/src/server.js
@@ -7,7 +7,7 @@ const PORT = 3002;
 
 // Configuración de CORS usando el paquete 'cors'
 const corsOptions = {
-  origin: 'http://52.206.95.187:80', // Permite solicitudes desde el frontend en este puerto
+  origin: '*', // Permite solicitudes desde el frontend en este puerto
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
   allowedHeaders: ['Content-Type'], // Cabeceras permitidas
   credentials: true, // Si estás utilizando cookies o autenticación con credenciales
