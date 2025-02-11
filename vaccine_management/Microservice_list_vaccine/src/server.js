65ac2a6feacd5fabbbf97b66e1e084d69c47a760
Karina1014 <janneth_wi15@hotmail.com>
2025-02-10 22:04:56 -0500

    feat: :sparkles: prove
    prove

--- vaccine_management/Microservice_list_vaccine/src/server.js
+++ vaccine_management/Microservice_list_vaccine/src/server.js
@@ -5,28 +5,21 @@ import cors from 'cors';
 const app = express();
 const PORT = 3002;
 
-// Configuración de CORS
-const allowedOrigins = ['http://34.227.117.50'];  // Asegúrate de agregar todas las IPs necesarias para tu frontend
-
+// Configuración simple de CORS para permitir solo una IP específica
 const corsOptions = {
-  origin: allowedOrigins,  // Permitir sólo las URLs del frontend específicas
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,  // Permite el manejo de credenciales (cookies, sesiones, etc.)
+  origin: 'http://34.227.117.50',  // Permite solicitudes solo desde esta IP
+  credentials: true,  // Permite el uso de credenciales como cookies
 };
 
-app.use(cors(corsOptions));  // Aplica CORS a todas las rutas
+app.use(cors(corsOptions));  // Aplica la configuración de CORS
 
 // Middleware para procesar JSON
 app.use(express.json());
 
 // Rutas de la API
-app.use('/api', vaccineRoute);  // Asegúrate de que '/api/vaccines' esté bien configurado en tu enrutador
+app.use('/api', vaccineRoute);  // Asegúrate de que '/api/vaccines' esté bien configurado
 
-// Manejo de solicitudes OPTIONS
-app.options('*', cors(corsOptions));  // Permite el manejo de las solicitudes preflight OPTIONS (esto es importante para las solicitudes con credenciales)
-
-// Verifica que el servidor esté corriendo
+// Inicia el servidor
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
