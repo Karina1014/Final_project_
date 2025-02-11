3efd6a1979f0c5342c27d4cc9a1ebaf3f55dc851
Karina1014 <janneth_wi15@hotmail.com>
2025-02-10 23:52:07 -0500

    feat: :sparkles: Prove 10
    prove changes

--- vaccine_management/Microservice_list_vaccine/src/server.js
+++ vaccine_management/Microservice_list_vaccine/src/server.js
@@ -7,31 +7,23 @@ const app = express();
 const PORT = 3002;
 
 connectDB();
-
+// Configuración de CORS
+const allowedOrigins = ['http://52.91.76.250']; 
 // Configuración de CORS
 const corsOptions = {
-  origin: 'http://34.227.117.50',  // Permite solo este origen
-  credentials: true,  // Permite el envío de cookies y autenticación
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
-  allowedHeaders: ['Content-Type'],  // Cabeceras permitidas
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
 };
-
 // Aplica el middleware de CORS
-app.use(cors(corsOptions));  // Aplica CORS antes de las rutas
+app.use(cors(corsOptions)); // Aplica CORS
 
 // Middleware para procesar JSON
 app.use(express.json());
 
 // Rutas de la API
 app.use('/api', vaccineRoute);
-
-// Middleware adicional para asegurar que las cabeceras CORS estén presentes en cada respuesta
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://34.227.117.50'); // Asegura que se permita el origen correcto
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 // Inicia el servidor
 app.listen(PORT, '0.0.0.0', () => {
