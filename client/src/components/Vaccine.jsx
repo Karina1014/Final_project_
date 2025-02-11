5efdc1454f04757d568e86f4f5fd733b12eacefd
Karina1014 <janneth_wi15@hotmail.com>
2025-02-10 23:21:04 -0500

    feat: :sparkles: Prove 6
    prove changes

--- client/src/components/Vaccine.jsx
+++ client/src/components/Vaccine.jsx
@@ -8,8 +8,8 @@ import 'react-toastify/dist/ReactToastify.css'; // CSS for Toastify
 
 const Vaccine = () => {
   const [data, setData] = useState([]); // State to store vaccine data
-  const [modalInsertar, setModalInsertar] = useState(false); // State to control the insert modal
-  const [modalActualizar, setModalActualizar] = useState(false); // State to control the update modal
+  const [modalInsert, setModalInsert] = useState(false); // State to control the insert modal
+  const [modalUpdate, setModalUpdate] = useState(false); // State to control the update modal
   const [form, setForm] = useState({
     id_vaccine: "", // Vaccine ID (used for editing and deleting)
     name: "", // Vaccine name
@@ -17,27 +17,23 @@ const Vaccine = () => {
     dose: "", // Dose of the vaccine
   });
 
-  const backendUrl = 'http://54.167.144.194';  // Asegúrate de que la IP del backend es correcta
-
-  const obtenerVacunas = async () => {
+  // Fetch vaccines from the backend
+  const fetchVaccines = async () => {
     try {
-      const response = await axios.get(`${backendUrl}:3002/api/vaccines`, {
-        withCredentials: true,  // Si necesitas enviar cookies o sesiones
+      const response = await axios.get('http://54.167.144.194:3002/api/vaccines', {
+        withCredentials: true
       });
-      console.log(response.data);
-      setData(response.data); // Guarda los datos en el estado
+
+      console.log(response.data); // Check data in console
+      setData(response.data); // Save data in state
     } catch (error) {
       toast.error("Error while fetching vaccines");
       console.error(error); // Log the error for debugging
     }
   };
-  
-  
-  
-  
 
   useEffect(() => {
-    obtenerVacunas(); // Fetch vaccines when the component mounts
+    fetchVaccines(); // Fetch vaccines when the component mounts
   }, []);
 
   // Handle form input changes
@@ -50,123 +46,122 @@ const Vaccine = () => {
   };
 
   // Show the modal for inserting a new vaccine
-  const mostrarModalInsertar = () => {
+  const showInsertModal = () => {
     setForm({
       id_vaccine: "",
       name: "",
       description: "",
       dose: "",
     });
-    setModalInsertar(true); // Open the insert modal
+    setModalInsert(true); // Open the insert modal
   };
 
   // Close the insert modal
-  const cerrarModalInsertar = () => {
-    setModalInsertar(false);
+  const closeInsertModal = () => {
+    setModalInsert(false);
   };
 
   // Show the modal for updating an existing vaccine
-  const mostrarModalActualizar = (dato) => {
-    setForm(dato); // Set the form with the data to be updated
-    setModalActualizar(true); // Open the update modal
+  const showUpdateModal = (data) => {
+    setForm(data); // Set the form with the data to be updated
+    setModalUpdate(true); // Open the update modal
   };
 
   // Close the update modal
-  const cerrarModalActualizar = () => {
-    setModalActualizar(false);
+  const closeUpdateModal = () => {
+    setModalUpdate(false);
   };
 
   // Insert a new vaccine into the database
-  const insertar = async () => {
+  const insert = async () => {
     try {
-      await axios.post(`${backendUrl}:3001/api/createVaccines`, form,{
-        withCredentials: true,  // Aquí está el objeto de configuración correcto
+      await axios.post('http://54.167.144.194:3001/api/createVaccines', form, {
+        withCredentials: true
       });
-      obtenerVacunas(); // Refresh the vaccine list
-      setModalInsertar(false); // Close the insert modal
+      fetchVaccines(); // Refresh the vaccine list
+      setModalInsert(false);
       toast.success("Vaccine inserted successfully");
     } catch (error) {
       toast.error("Error while inserting vaccine");
-      console.error(error); // Log the error for debugging
+      console.error(error);
     }
   };
 
   // Edit an existing vaccine
-  const editar = async () => {
+  const update = async () => {
     try {
-      const response = await axios.put(`${backendUrl}:3004/api/updateVaccines`, form,
-        {
-          withCredentials: true,  // Aquí está el objeto de configuración correcto
-        });
+      const response = await axios.put('http://54.167.144.194:3004/api/updateVaccines', form, {
+        withCredentials: true
+      });
+
       if (response.data) {
         toast.success("Vaccine updated successfully");
-        obtenerVacunas(); // Refresh the vaccine list
-        setModalActualizar(false); // Close the update modal
+        fetchVaccines(); // Refresh the vaccine list
+        setModalUpdate(false);
       } else {
         toast.error("Error while updating vaccine");
       }
     } catch (error) {
       toast.error("Error while updating vaccine");
-      console.error(error); // Log the error for debugging
+      console.error(error); // Log for debugging
     }
   };
 
   // Delete a vaccine
-  const eliminar = async (id_vaccine) => {
+  const deleteVaccine = async (id_vaccine) => {
     try {
-      const response = await axios.delete(`${backendUrl}:3003/api/deleteVaccine/${id_vaccine}`,
-      {
-        withCredentials: true,  // Aquí está el objeto de configuración correcto
+      const response = await axios.delete(`http://54.167.144.194:3003/api/deleteVaccine/${id_vaccine}`, {
+        withCredentials: true
       });
       if (response.data) {
         toast.success("Vaccine deleted successfully");
-        obtenerVacunas(); // Refresh the vaccine list
+        fetchVaccines(); // Refresh the vaccine list
       } else {
         toast.error("Error while deleting vaccine");
       }
     } catch (error) {
       toast.error("Error while deleting vaccine");
-      console.error(error); // Log the error for debugging
+      console.error(error); // Log for debugging
     }
   };
 
   return (
     <>
-      <Button color="success" onClick={mostrarModalInsertar}>Insert New Vaccine</Button>
+      <Button color="success" onClick={showInsertModal}>Insert New Vaccine</Button>
       <div className="mt-4">
-      <Table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Dose</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((vaccine) => (
-            <tr key={vaccine.id_vaccine}>
-              <td>{vaccine.id_vaccine}</td> {/* Display vaccine ID */}
-              <td>{vaccine.name}</td> {/* Display vaccine name */}
-              <td>{vaccine.description}</td> {/* Display vaccine description */}
-              <td>{vaccine.dose}</td> {/* Display vaccine dose */}
-              <td>
-                <Button color="primary" onClick={() => mostrarModalActualizar(vaccine)}>
-                  <FaEdit /> {/* Icon to edit vaccine */}
-                </Button>
-                <Button color="danger" className="ml-2" onClick={() => eliminar(vaccine.id_vaccine)}>
-                  <FaTrashAlt /> {/* Icon to delete vaccine */}
-                </Button>
-              </td>
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Dose</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {data.map((vaccine) => (
+              <tr key={vaccine.id_vaccine}>
+                <td>{vaccine.id_vaccine}</td> {/* Display vaccine ID */}
+                <td>{vaccine.name}</td> {/* Display vaccine name */}
+                <td>{vaccine.description}</td> {/* Display vaccine description */}
+                <td>{vaccine.dose}</td> {/* Display vaccine dose */}
+                <td>
+                  <Button color="primary" onClick={() => showUpdateModal(vaccine)}>
+                    <FaEdit /> {/* Icon to edit vaccine */}
+                  </Button>
+                  <Button color="danger" className="ml-2" onClick={() => deleteVaccine(vaccine.id_vaccine)}>
+                    <FaTrashAlt /> {/* Icon to delete vaccine */}
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
 
       {/* Insert Modal */}
-      <Modal isOpen={modalInsertar}>
+      <Modal isOpen={modalInsert}>
         <ModalHeader>Insert Vaccine</ModalHeader>
         <ModalBody>
           <FormGroup>
@@ -183,13 +178,13 @@ const Vaccine = () => {
           </FormGroup>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={insertar}>Insert</Button>
-          <Button color="secondary" onClick={cerrarModalInsertar}>Cancel</Button>
+          <Button color="primary" onClick={insert}>Insert</Button>
+          <Button color="secondary" onClick={closeInsertModal}>Cancel</Button>
         </ModalFooter>
       </Modal>
 
       {/* Update Modal */}
-      <Modal isOpen={modalActualizar}>
+      <Modal isOpen={modalUpdate}>
         <ModalHeader>Update Vaccine</ModalHeader>
         <ModalBody>
           <FormGroup>
@@ -206,8 +201,8 @@ const Vaccine = () => {
           </FormGroup>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={editar}>Update</Button>
-          <Button color="secondary" onClick={cerrarModalActualizar}>Cancel</Button>
+          <Button color="primary" onClick={update}>Update</Button>
+          <Button color="secondary" onClick={closeUpdateModal}>Cancel</Button>
         </ModalFooter>
       </Modal>
 
