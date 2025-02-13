718f9f6e6529c621ff48b161d183158ded8703a1
Karina1014 <janneth_wi15@hotmail.com>
2025-02-13 14:01:24 -0500

    feat: :sparkles: add withCredentials
    add withCredentials

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 export const AppContent = createContext();
 
 export const AppContextProvider = (props) => {
-  
+
   axios.defaults.withCredentials = true; // Asegurarse de que las cookies se envíen correctamente.
 
   const [isLoggedin, setIsLoggedin] = useState(false);
@@ -14,20 +14,30 @@ export const AppContextProvider = (props) => {
   // Verifica el estado de autenticación
   const getAuthState = async () => {
     try {
-      const { data } = await axios.get(`http://52.72.179.181:6005/api/auth/is-auth`);
+      const { data } = await axios.get(`http://52.72.179.181:6005/api/auth/is-auth`, {
+        withCredentials: true // Asegura que las cookies se envíen
+      });
+      
       if (data.success) {
         setIsLoggedin(true);
         getUserData();
-      } 
+      } else {
+        setIsLoggedin(false); // Si no está autenticado, actualiza el estado
+      }
     } catch (error) {
-      toast.error( error.message); // Mejor manejo de errores
+      toast.error(error.message); // Mejor manejo de errores
     }
   };
 
   // Obtener los datos del usuario
   const getUserData = async () => {
     try {
-      const { data } = await axios.get(`http://52.72.179.181:6005/api/user/data`);
+      const { data } = await axios.get(`http://52.72.179.181:6005/api/user/data`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}` // Si tienes el token guardado en localStorage
+        }
+      });
+
       if (data.success) {
         setUserData(data.userData);
       } else {
