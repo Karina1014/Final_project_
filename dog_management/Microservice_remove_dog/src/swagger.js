87f8931a13a6446148cfbcba6022e2224eb2ce6b
Jesdhy <pinchajessy529@gmail.com>
2025-02-14 04:15:13 -0500

    docs: :memo: update swagger
    Add required fields, improve documentation and description

--- dog_management/Microservice_remove_dog/src/swagger.js
+++ dog_management/Microservice_remove_dog/src/swagger.js
@@ -6,10 +6,39 @@ const endpointsFiles = ['./routes/dogRouter.js'];
 const doc = {
     info: {
         title: 'Dog API',
-        description: 'API for remove dogs',
+        description: 'API for removing dogs',
     },
     host: 'localhost:4002',
     schemes: ['http', 'https'],
+    paths: {
+        "/remove/{id}": {
+            "delete": {
+                "description": "Deletes a dog by its ID",
+                "parameters": [
+                    {
+                        "name": "id",
+                        "in": "path",
+                        "required": true,
+                        "description": "ID of the dog to delete",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Dog successfully deleted"
+                    },
+                    "400": {
+                        "description": "Invalid ID supplied"
+                    },
+                    "404": {
+                        "description": "Dog not found"
+                    }
+                }
+            }
+        }
+    }
 };
 
 swaggerAutogen()(outputFile, endpointsFiles, doc);
