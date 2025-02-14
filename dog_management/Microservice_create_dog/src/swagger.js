c81531233dbf9712c6e28347ac9d2cd32efb3137
Jesdhy <pinchajessy529@gmail.com>
2025-02-14 03:58:36 -0500

    docs: :memo: update swagger
    Add required fields, improve documentation and description

--- dog_management/Microservice_create_dog/src/swagger.js
+++ dog_management/Microservice_create_dog/src/swagger.js
@@ -4,13 +4,27 @@ const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/dogRouter.js'];
 
 const doc = {
+    swagger: '2.0', 
     info: {
         title: 'Dog API',
-        description: 'API for creating dog',
+        description: 'API for managing dog records',
+        version: '1.0.0',
     },
-
     host: 'localhost:4000',
+    basePath: '/api/dogs',
     schemes: ['http', 'https'],
+    definitions: {
+        Dog: {
+            type: 'object',
+            required: ['name', 'breed', 'age', 'gender'],
+            properties: {
+                name: { type: 'string', example: 'Buddy' },
+                breed: { type: 'string', example: 'Labrador' },
+                age: { type: 'integer', example: 3 },
+                gender: { type: 'string', enum: ['Male', 'Female'], example: 'Male' }
+            }
+        }
+    }
 };
 
 swaggerAutogen()(outputFile, endpointsFiles, doc);
